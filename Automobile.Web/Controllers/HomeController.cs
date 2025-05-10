7e28932fe35406543533d5113a215a7a8aba8157
dineshkumarsathasivam1011 <144995113+VasanthPranmalai10@users.noreply.github.com>
2025-05-10 17:04:18 +0530

    Add project files.

---
+++ Automobile.Web/Controllers/HomeController.cs
@@ -0,0 +1,32 @@
+using System.Diagnostics;
+using Automobile.Web.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Automobile.Web.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly ILogger<HomeController> _logger;
+
+        public HomeController(ILogger<HomeController> logger)
+        {
+            _logger = logger;
+        }
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult Privacy()
+        {
+            return View();
+        }
+
+        [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+        public IActionResult Error()
+        {
+            return View(new ErrorViewModel { RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier });
+        }
+    }
+}
---
+++ Automobile.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Automobile.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
