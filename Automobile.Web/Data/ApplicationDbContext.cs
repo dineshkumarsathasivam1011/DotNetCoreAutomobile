8cf1c258f8946717c2b9be80d8cb0529a1aca402
dineshkumarsathasivam1011 <144995113+VasanthPranmalai10@users.noreply.github.com>
2025-05-13 02:13:07 +0530

    Automonile First Push

---
+++ Automobile.Web/Data/ApplicationDbContext.cs
@@ -0,0 +1,16 @@
+﻿using Automobile.Web.Models;
+using Microsoft.EntityFrameworkCore;
+
+namespace Automobile.Web.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+            
+        }
+
+        public DbSet<Brand> BrandAutomobile { get; set; }
+
+    }
+}
